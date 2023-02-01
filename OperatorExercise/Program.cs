2e532adc39c248a4a7d922cd10bbda623d690032
DavidRose <DavidRose331@Gmail.com>
2023-02-01 12:21:49 -0600

    operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace OperatorExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+            //For division, create two integer variables
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+            
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(Methods.AreaOfCircle(radius));
+
+            Methods.AreaOfCircle(radius);
+
+        }   
+
+
+        
+          
+        
+
+
+
+
+
+    } 
 }
+
